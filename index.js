cbd4dbd2e6594c787ae3be03039d3010f58f4194
S4V10N <didiapraisechimeka@gmail.com>
2025-04-30 14:15:05 +0100

    fixed version

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
+const form = document.getElementById('form');
+const formSection = document.getElementById('form-section');
+const success = document.getElementById('success');
 const input = document.getElementById('email');
 const errorMsg = document.querySelector('.error');
-const form = document.getElementById('form');
-
 const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-
-
-document.addEventListener('DOMContentLoaded' , function(){
-    const form = document.getElementById('form');
-    const formSection = document.getElementById('form-section');
-    const success = document.getElementById('success'); 
-
-    form.addEventListener('submit' , function(event){
-        event.preventDefault();
-        if (pattern.test(input.value) && input.value.lenght < 1) {
-          errorMsg.style.display = 'none';
-          input.style.border = '';
-          input.style.backgroundColor = '';
-          input.style.color = '';
-        } else {
-          formSection.style.display = 'none';
-          success.style.display = 'block';
-  
-          setTimeout(function() {
-              location.reload();
-          }, 3000)
-        }
-
-    })
-});
-
+form.addEventListener('submit' , function(event) {
+  event.preventDefault();
+  if (!pattern.test(input.value) ?? input.value.length < 1) {
+    errorMsg.style.display = 'block';
+    input.style.border = '2px solid red';
+    input.style.backgroundColor = 'hsla(4, 100%, 67%, 0.5)';
+    input.style.color = 'red'
+  } else {
+    formSection.style.display = 'none';
+    success.style.display = 'block';
+  }
+})
