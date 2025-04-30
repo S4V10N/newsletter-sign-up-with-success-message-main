a9e9cc736985d69f5022fd949ab15bd25299a429
S4V10N <didiapraisechimeka@gmail.com>
2025-04-30 11:16:51 +0100

    cry for help

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded' , function(){
-    const form = document.getElementById('form');
-    const formSection = document.getElementById('form-section');
-    const success = document.getElementById('success'); 
-
-    form.addEventListener('submit' , function(event){
-        event.preventDefault();
-
-        formSection.style.display = 'none';
-        success.style.display = 'block';
-
-        setTimeout(function() {
-            location.reload();
-        }, 3000)
-    })
-});
-
-
 const input = document.getElementById('email');
 const errorMsg = document.querySelector('.error');
 const form = document.getElementById('form');
@@ -23,28 +5,28 @@ const form = document.getElementById('form');
 const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
-input.addEventListener('input', () => {
-  if (pattern.test(input.value)) {
-    errorMsg.style.display = 'none';
-    input.style.border = '';
-    input.style.backgroundColor = '';
-    input.style.color = '';
-  } else {
-    errorMsg.style.display = 'block';
-    input.style.border = '2px solid red';
-    input.style.backgroundColor = 'hsla(4, 100%, 67%, 0.5)';
-    input.style.color = 'red';
-  }
+
+document.addEventListener('DOMContentLoaded' , function(){
+    const form = document.getElementById('form');
+    const formSection = document.getElementById('form-section');
+    const success = document.getElementById('success'); 
+
+    form.addEventListener('submit' , function(event){
+        event.preventDefault();
+        if (pattern.test(input.value) && input.value.lenght < 1) {
+          errorMsg.style.display = 'none';
+          input.style.border = '';
+          input.style.backgroundColor = '';
+          input.style.color = '';
+        } else {
+          formSection.style.display = 'none';
+          success.style.display = 'block';
+  
+          setTimeout(function() {
+              location.reload();
+          }, 3000)
+        }
+
+    })
 });
 
-form.addEventListener('submit', (e) => {
-    if (!pattern.test(input.value)) {
-      errorMsg.style.display = 'block';
-      input.style.border = '2px solid red';
-      input.style.backgroundColor = 'hsla(4, 100%, 67%, 0.5)';
-      input.style.color = 'red';
-      e.preventDefault();
-    } else {
-
-    }
-  });
