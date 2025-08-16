9f36112b2f3e829495191d0bc9858876c0117f17
savion <didiapraisechimeka@gmail.com>
2025-08-16 13:45:26 +0100

    help!!!!!!

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const formSection = document.getElementById('form-section');
 const success = document.getElementById('success');
 const input = document.getElementById('email');
 const errorMsg = document.querySelector('.error');
+const hideBtn = document.getElementById('hide-msg');
 const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-form.addEventListener('submit' , function(event) {
+form.addEventListener('submit', function(event) {
   event.preventDefault();
-  if (!pattern.test(input.value) ?? input.value.length < 1) {
+
+  if (!pattern.test(input.value) || input.value.trim().length < 1) {
     errorMsg.style.display = 'block';
     input.style.border = '2px solid red';
     input.style.backgroundColor = 'hsla(4, 100%, 67%, 0.5)';
-    input.style.color = 'red'
+    input.style.color = 'red';
   } else {
-    formSection.style.display = 'none';
-    success.style.display = 'block';
+    errorMsg.classList.add("none");
+    formSection.classList.add("none");
+    success.classList.remove("none");
+
+    input.value = '';
   }
-})
+});
+
+hideBtn.addEventListener('click', function() {
+  success.classList.add("none");
+  formSection.classList.remove("none");
+});
